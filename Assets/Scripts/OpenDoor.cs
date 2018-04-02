ab6fbba74acb0c31ad948b91fae24704b420bf15
DESKTOP-IQ01BMT\JoJo <biohazerd8@gmail.com>
2018-04-01 22:47:49 -0400

    Lerps and levels and stuff

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -8,16 +8,20 @@ public class OpenDoor : MonoBehaviour
     public GameObject doorClose;
     public GameObject doorOpenPosition;
 
+    public Vector3 openPosition;
+
     public bool openDoor = false;
     public bool closeDoor = false;
     public bool isOpen;
     public bool isClosed;
 
     public float speed;
+    public float timer;
 
     private void Start()
     {
         doorOpenPosition.transform.position = door.transform.position;
+        openPosition = door.transform.position;
     }
 
     private void Update()
@@ -52,23 +56,30 @@ public class OpenDoor : MonoBehaviour
 
     IEnumerator doorOpen()
     {
+        timer += Time.deltaTime;
         Debug.Log("Door open Coroutine started");
-        door.transform.position = Vector3.MoveTowards(door.transform.position, doorClose.transform.position *.95f, speed * Time.deltaTime);
+        //door.transform.position = Vector3.MoveTowards(door.transform.position, doorClose.transform.position *.95f, speed * Time.deltaTime);
+        door.transform.position = Vector3.Lerp(openPosition, doorClose.transform.position, timer);
         yield return new WaitForSeconds(1);
         openDoor = false;
         isClosed = true;
         Debug.Log("Open door is false");
+        timer = 0;
         yield return null;
     }
 
     IEnumerator doorCloseFunc()
     {
+        timer += Time.deltaTime;
         Debug.Log("Door close Coroutine started");
-        door.transform.position = Vector3.MoveTowards(door.transform.position, doorClose.transform.position, -speed * Time.deltaTime);
+        //door.transform.position = Vector3.MoveTowards(door.transform.position, doorClose.transform.position, -speed * Time.deltaTime);
+        door.transform.position = Vector3.Lerp(doorClose.transform.position, openPosition, timer);
         yield return new WaitForSeconds(1);
         closeDoor = false;
         isOpen = true;
+        isClosed = false;
         Debug.Log("close door is false");
+        timer = 0;
         yield return null;
     }
 }
