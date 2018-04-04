3b9433042e25e61d457c821a6d65ba243d352c2b
Kevin Huang <Heilong1998@gmail.com>
2018-04-03 23:07:51 -0400

    Stuffs
    Now the powerups are one prefab
    Called Powerup, just check the bool if you want it to be reverse powerup.
    It defaults or Sync
    Player manager now dictates both players, no need to have another script on player 2
    Just checkmark the box, start reversed to start them out already reversed.
    Movement speed modified to be more smooth and not jump through walls.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    public GameObject p1;
+    public GameObject p2;
     public float moveSpeed;
-    
+    public bool isReversed = false;
+
+    private void Start()
+    {
+        p1 = GameObject.Find("Player 1");
+        p2 = GameObject.Find("Player 2");
+    }
+
     void FixedUpdate()
     {
         Movement();
     }
 
+    public void Reverse()
+    {
+        isReversed = true;
+    }
+    public void Syncronize()
+    {
+        isReversed = false;
+    }
+
     void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        float vertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        if (isReversed == true)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float vertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
 
-        transform.Translate(horizontal, vertical,0);
+            p1.transform.Translate(horizontal, vertical, 0);
+            p2.transform.Translate(-horizontal, -vertical, 0);
+        }
+        else
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float vertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+            p1.transform.Translate(horizontal, vertical, 0);
+            p2.transform.Translate(horizontal, vertical, 0);
+        }
     }
 }
--- Assets/Scripts/PowerupManager.cs
+++ Assets/Scripts/PowerupManager.cs
@@ -4,42 +4,26 @@ using UnityEngine;
 
 public class PowerupManager : MonoBehaviour
 {
-    public bool isReversePowerup;
-    public bool movementEnabled = false;
-    public bool StartReversed;
-
-    public GameObject player1;
-    public GameObject player2;
+    public GameObject player;
+    public bool ReversePowerup = false;
 
     public void Start()
     {
-        player1 = GameObject.FindGameObjectWithTag("Player 1");
-        player2 = GameObject.FindGameObjectWithTag("Player 2");
-        
-        if (StartReversed == true)
-        {
-            ReverseMovement();
-        }
+        player = GameObject.FindGameObjectWithTag("Player 1");
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player 1") || other.CompareTag("Player 2"))
         {
-            if (isReversePowerup)
+            if(ReversePowerup == false)
             {
-                ReverseMovement();
+                player.GetComponent<PlayerManager>().Syncronize();
+            }
+            else
+            {
+                player.GetComponent<PlayerManager>().Reverse();
             }
         }
-
-        if (other.CompareTag("Player 1") && !movementEnabled)
-        {
-            player2.GetComponent<PlayerManager>().enabled = true;
-        }
-    }
-
-    public void ReverseMovement()
-    {
-        player1.GetComponent<PlayerManager>().moveSpeed = player1.GetComponent<PlayerManager>().moveSpeed * -1;
     }
 }
