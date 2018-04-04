f276877743e8a58c9a64a952906bdd00b120cbcb
Joshua Nieves <joshuanieves08@gmail.com>
2018-04-04 19:26:00 -0400

    Win Condition
    Added win trigger that enables "Win Canvas" that then allows player to move do a variety of things

---
+++ Assets/WinTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject winCanvas;
+
+    private void Start()
+    {
+        winCanvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player 1") || other.CompareTag("Player 2"))
+        {
+            winCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
